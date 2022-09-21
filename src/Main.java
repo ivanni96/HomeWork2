6335da845326bb337148090114bb68b5b8ade06a
ivan <ivanni95@gmail.com>
2022-09-21 18:12:21 +0400

    Выполнил действия с переменной frog (задача 5)

--- src/Main.java
+++ src/Main.java
@@ -24,7 +24,14 @@ var paper = 763789.0;
         System.out.println (friend);
         friend = friend / 7;
         System.out.println (friend);
-
+        var frog = 3.5;
+        System.out.println (frog);
+        frog = frog * 10;
+        System.out.println (frog);
+        frog = frog / 3.5;
+        System.out.println (frog);
+        frog = frog + 4;
+        System.out.println (frog);
     }
 
 
